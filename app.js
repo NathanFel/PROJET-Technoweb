6073f9cb6c228d040eaaf225f11553d67ba039d4
NathanFel <106067450+NathanFel@users.noreply.github.com>
2022-12-20 23:54:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,143 +6,140 @@ const client = new MongoClient(process.env.MONGO_URL)
 async function main(){
     await client.connect();
     console.log('Connection OK!');
-    const db =client.db('myTask');
-    const collection =db.collection('documents');
+    const db =client.db('dataInsta');
+    const collection =db.collection('users');
     /*const insertStuff=await collection.insertMany([{a: 1} , {b: 2} ,{c: 3}]);
     console.log('Docuements insérés => ${inseertStuff}');*/
     
-    /*Create
-    try{
+    //Create
+   /* try{
         const insertData=await collection.insertMany([
             {
                 name : 'Emma',
                 age : 15,
                 sexe : 'F',
-                country : 'Etats-Unis',
+                country : 'Etats-Unis'
             },
             {
                 name : 'Ines',
                 age : 20,
                 sexe : 'F',
-                country : 'Espagne',
+                country : 'Espagne'
             },
             {
                 name : 'Jules',
-                age : 26, 
+                age : 26,
                 sexe : 'H',
-                country : 'France',
+                country : 'France'
             },
             {
                 name : 'Alexandre',
                 age : 45,
                 sexe : 'Autre',
-                country : 'Slovaquie',
+                country : 'Slovaquie'
             },
             {
                 name : 'Lucas',
                 age : 13,
                 sexe : 'Autre',
-                country : 'Japon',
+                country : 'Japon'
             },
             {
                 name : 'Antonin',
                 age : 27,
                 sexe : 'H',
-                country : 'France',
+                country : 'France'
             },
             {
                 name : 'Gaston',
                 age : 55,
                 sexe : 'H',
-                country : 'Bénin',
+                country : 'Bénin'
             },
             {
                 name : 'Selima',
                 age : 23,
                 sexe : 'F',
-                country : 'Tunisie',
+                country : 'Tunisie'
             },
             {
                 name : 'Mathis',
                 age : 37,
                 sexe : 'H',
-                country : 'Vietnam',
+                country : 'Vietnam'
             },
             {
                 name : 'Ilona',
                 age : 16,
                 sexe : 'F',
-                country : 'Etats-Unis',
+                country : 'Etats-Unis'
             },
             {
                 name : 'Tom',
                 age : 32,
                 sexe : 'H',
-                country : 'Belgique',
+                country : 'Belgique'
             },
             {
                 name : 'Gunter',
                 age : 24,
                 sexe : 'H',
-                country : 'Allemagne',
+                country : 'Allemagne'
             },
             {
                 name : 'Esteban',
                 age : 25,
                 sexe : 'H',
-                country : 'Espagne',
+                country : 'Espagne'
             },
             {
                 name : 'Juan',
                 age : 26,
                 sexe : 'H',
-                country : 'Espagne',
+                country : 'Espagne'
             },
             {
                 name : 'Jean',
                 age : 27,
                 sexe : 'H',
-                country : 'France',
+                country : 'France'
             },
             {
                 name : 'Jane',
                 age : 28,
                 sexe : 'F',
-                country : 'Angleterre',
+                country : 'Angleterre'
             },
             {
                 name : 'Ceryss',
                 age : 17,
                 sexe : 'F',
-                country : 'Angleterre',
+                country : 'Angleterre'
             },
             {
                 name : 'Pierre',
                 age : 31,
                 sexe : 'H',
-                country : 'France',
+                country : 'France'
             },        
             {
                 name : 'Jeanne',
                 age : 19,
                 sexe : 'F',
-                country : 'France',
+                country : 'France'
             }
         ]);
         console.log('Docuements insérés =>', insertData);
     }catch(e) {throw e; }
-    */
-
+    
+*/
     //Read
     try{
         const findMultipleData=await collection.find({ country: 'France'});         
         console.log(await findMultipleData.toArray());
     }catch(e) {throw e; }
-    
-
-
     return 'done!';
-} 
+}
 
 main()
 .then(console.log)
